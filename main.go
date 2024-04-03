ca0b3fbf886c2d4b627ee3e6e04d904c73049f99
Bijan Haney <bijan@viam.com>
2024-04-03 17:44:45 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+// Package main is a module which serves the ocean-prefilter vision service
+package main
+
+import (
+	"context"
+
+	"go.viam.com/rdk/services/vision"
+
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/module"
+	"go.viam.com/utils"
+
+	"github.com/viam-labs/ocean-prefilter/ocean_prefilter"
+)
+
+func main() {
+	// NewLoggerFromArgs will create a logging.Logger at "DebugLevel" if
+	// "--log-level=debug" is an argument in os.Args and at "InfoLevel" otherwise.
+	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs("ocean-prefilter"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
+	myMod, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	// Models and APIs add helpers to the registry during their init().
+	// They can then be added to the module here.
+	err = myMod.AddModelFromRegistry(ctx, vision.API, ocean_prefilter.Model)
+	if err != nil {
+		return err
+	}
+
+	err = myMod.Start(ctx)
+	defer myMod.Close(ctx)
+	if err != nil {
+		return err
+	}
+	<-ctx.Done()
+	return nil
+}
